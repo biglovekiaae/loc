5a2095c9ecfafcb2c4dad091969edb3255f59dcf
biglovekiaae <57329384+biglovekiaae@users.noreply.github.com>
2020-07-06 17:17:15 +0700

    Update index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<h1> Hello</h1>
+</head>
+<body>
+<h1>bang cuu chuong x7 ne</h1>
+<?php
+for ($i = 1; $i < 10; $i++) {
+echo"7x" . $i . " = " . 7 * $i ."<br>";
+}
+?>
+</body>
+</html>
